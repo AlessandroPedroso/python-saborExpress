92742c94c546ba183aae7cf13237cf03e34f15e6
Your Name <alessanrospedroso@outlook.com>
2024-08-02 15:18:21 -0300

    primeiro commit

---
+++ Exercicios/1.Modulo/1.Exercicio.py
@@ -0,0 +1,8 @@
+# // 1 - Para verificar se um número é impar ou par, você pode fazer essa estrutura condicional:
+
+numero = int(input("Digite um número "))
+
+if numero % 2 == 0:
+    print('o número é par.')
+else:
+    print('o número é impar.')
